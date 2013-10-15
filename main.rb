30ee72221d75685c172859366c26ef54797ec192
Kristen Boydstun <kristen.boydstun@gmail.com>
2013-10-14 23:16:23 -0700

    Save player name to session

--- main.rb
+++ main.rb
@@ -4,7 +4,20 @@ require 'sinatra'
 set :sessions, true
 
 get '/' do
-  erb :whatsyourname
+  if session[:player_name]
+    # do something
+  else
+    redirect '/new_game'
+  end
+end
+
+get '/new_game' do
+  erb :new_game
 end
 
 
+post '/start_game' do
+  session[:player_name]=params[:player_name]
+  @player_name = session[:player_name]
+  erb :start_game
+end
