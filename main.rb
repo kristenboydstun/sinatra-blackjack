ebda65de4120042a1765129ed6e86a7fa4c1d522
Kristen Boydstun <kristen.boydstun@gmail.com>
2013-10-20 01:40:03 -0700

    Added card image display and styled

--- main.rb
+++ main.rb
@@ -5,13 +5,14 @@ set :sessions, true
 
 helpers do
   def countCards cards
-    valueDictionary = {"ace" => 11, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9, "ten" => 10, "jack" => 10, "queen" => 10, "king" => 10}
+    valueDictionary = {"ace" => 11, "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10, "jack" => 10, "queen" => 10, "king" => 10}
     total = 0
     aces = 0
     
     names = cards.map { |name, suit| name }
 
     names.each do |name| 
+      puts name
       total += valueDictionary["#{name}"]
       aces = aces + 1 if name == "ace"
     end
@@ -23,7 +24,7 @@ helpers do
   end
 
   def getImage card
-    
+    "<img src='/images/cards/#{card[1]}_#{card[0]}.jpg'>"
   end
 end
 
@@ -48,8 +49,8 @@ end
 
 get '/initialize' do
   suits = ["hearts","spades","diamonds","clubs"]
-  types = ["ace", "two", "three", "four", "five", "six", "seven",
-              "eight", "nine", "ten", "jack", "queen", "king"]
+  types = ["ace", "2", "3", "4", "5", "6", "7",
+              "8", "9", "10", "jack", "queen", "king"]
 
   session[:deck] = types.product(suits).shuffle!
   session[:dealer_hand]=[]
@@ -58,6 +59,8 @@ get '/initialize' do
   session[:dealer_hand]<<session[:deck].pop
   session[:player_hand]<<session[:deck].pop
   session[:player_hand]<<session[:deck].pop
+
+  getImage(session[:deck].pop)
 
   session[:game_over] = false
   session[:player_turn] = true
